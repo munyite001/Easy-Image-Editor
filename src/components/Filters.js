53b0334bb01b52153cf61e9a2ef8419b14243f8d
Emmanuel Munyite <emunyite@gmail.com>
2023-01-07 08:18:23 +0300

    design review

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -7,17 +7,17 @@ export default function Filters()
       <div className="edit-panel">
         <h2>Filters</h2>
         <div className="options">
-          <button className="active">Brightness</button>
-          <button>Saturation</button>
-          <button>Inversion</button>
-          <button>grayscale</button>
+          <button className="active filter-btn">Brightness</button>
+          <button className="filter-btn">Saturation</button>
+          <button className="filter-btn">Inversion</button>
+          <button className="filter-btn">grayscale</button>
         </div>
         <div className="filter-info">
           <p className="name">Brightness</p>
           <p className="value">100%</p>
         </div>
         <div className="slider">
-          <input type ="range" value="100" min="0" max="200"/>
+          <input type ="range" defaultValue="100" min="0" max="200"/>
         </div>
         <div className="rotate">
           <h2>Rotate & Flip</h2>
